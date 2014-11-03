de896a9e315b9b63b7f45096b6635d4e8d7cbb6f
Kevin Mannix <kmannix@bu.edu>
2014-11-03 17:29:59 -0500

    Initialized the XML portion of the app for the EC 327 talk

---
+++ TipCalculator/src/me/kmannix/tipcalculator/MainActivity.java
@@ -0,0 +1,17 @@
+package me.kmannix.tipcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
